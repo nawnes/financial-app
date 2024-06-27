0b6f7c1911c41fd0ee5e073123497b9f4f6570c2
amalmrt <amal.mernit@hotmail.com>
2024-06-26 23:24:23 -0400

    test

--- client/src/components/BalanceOverview.vue
+++ client/src/components/BalanceOverview.vue
@@ -2,18 +2,28 @@
   <div>
     <h2>Balance Overview</h2>
     <div>
-      <strong>Total Income:</strong> {{ totalIncome }}
+      <label for="currency-select">Select Currency:</label>
+      <select id="currency-select" v-model="selectedCurrency" @change="updateCurrency">
+        <option v-for="(rate, currency) in exchangeRates" :key="currency" :value="currency">
+          {{ currency }}
+        </option>
+      </select>
     </div>
     <div>
-      <strong>Total Expense:</strong> {{ totalExpense }}
+      <strong>Total Income:</strong> {{ convertedTotalIncome }} {{ selectedCurrency }}
     </div>
     <div>
-      <strong>Net Balance:</strong> {{ netBalance }}
+      <strong>Total Expense:</strong> {{ convertedTotalExpense }} {{ selectedCurrency }}
+    </div>
+    <div>
+      <strong>Net Balance:</strong> {{ convertedNetBalance }} {{ selectedCurrency }}
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'BalanceOverview',
   props: {
@@ -21,6 +31,14 @@ export default {
       type: Array,
       required: true
     }
+  },
+  data() {
+    return {
+      selectedCurrency: 'USD',
+      exchangeRates: {},
+      baseCurrency: 'USD',
+      conversionRates: {}
+    };
   },
   computed: {
     totalIncome() {
@@ -35,6 +53,34 @@ export default {
     },
     netBalance() {
       return this.totalIncome - this.totalExpense;
+    },
+    convertedTotalIncome() {
+      return (this.totalIncome * this.conversionRates[this.selectedCurrency]).toFixed(2);
+    },
+    convertedTotalExpense() {
+      return (this.totalExpense * this.conversionRates[this.selectedCurrency]).toFixed(2);
+    },
+    convertedNetBalance() {
+      return (this.netBalance * this.conversionRates[this.selectedCurrency]).toFixed(2);
+    }
+  },
+  async created() {
+    await this.fetchExchangeRates();
+  },
+  methods: {
+    async fetchExchangeRates() {
+      try {
+        const response = await axios.get('https://v6.exchangerate-api.com/v6/ff81c9652bd26e2508fef927/latest/USD');
+        this.exchangeRates = response.data.conversion_rates;
+        this.conversionRates = { ...this.exchangeRates, USD: 1 };
+      } catch (error) {
+        console.error("Failed to fetch exchange rates", error);
+      }
+    },
+    async updateCurrency() {
+      if (!this.exchangeRates[this.selectedCurrency]) {
+        await this.fetchExchangeRates();
+      }
     }
   }
 };
@@ -43,5 +89,16 @@ export default {
 <style scoped>
 div {
   margin-bottom: 10px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+select {
+  margin-bottom: 10px;
+  padding: 5px;
+  font-size: 16px;
 }
 </style>
