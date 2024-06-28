8071c770ded49b47b0b1a920b494d63f2d1828e2
nawnes <anas.baroudi03@gmail.com>
2024-06-28 04:20:14 +0200

    nav bar dynamique

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
     <nav>
+      <div class="logo-container">
+        <img src="@/assets/logo-projet.png" alt="Logo" class="logo">
+      </div>
       <div class="nav-items montserrat-font">
-        <router-link to="/">Home</router-link>
-        <router-link to="/add-transaction">Add Transaction</router-link>
-        <router-link v-if="isAuthenticated" to="/profile">Profile</router-link>
         <router-link to="/about">About</router-link>
+        <router-link v-if="isAuthenticated" to="/">Home</router-link>
+        <router-link v-if="isAuthenticated" to="/add-transaction">Add Transaction</router-link>
+        <router-link v-if="isAuthenticated" to="/profile">Profile</router-link>
+        <router-link v-if="isAuthenticated" to="/about">About</router-link>
         <button v-if="isAuthenticated" @click="logout">Logout</button>
         <router-link v-else to="/login">Login</router-link>
-        
       </div>
     </nav>
     <router-view />
@@ -26,6 +29,7 @@ export default {
     logout() {
       localStorage.removeItem('token');
       this.$router.push('/login');
+      location.reload();
     }
   }
 };
@@ -38,10 +42,10 @@ body {
   font-family: 'Montserrat', sans-serif;
   margin: 0;
   padding: 0;
-  background-image: url('assets/gradient_1.jpg'); /* Chemin vers l'image d'arrière-plan */
-  background-size: cover; /* Couvrir l'ensemble de la page */
-  background-repeat: no-repeat; /* Ne pas répéter l'image */
-  background-attachment: fixed; /* Image d'arrière-plan fixe */
+  background-image: url('assets/gradient_1.jpg');
+  background-size: cover; 
+  background-repeat: no-repeat; 
+  background-attachment: fixed; 
 }
 
 .nav-items {
@@ -57,7 +61,8 @@ body {
 nav {
   display: flex;
   justify-content: flex-start;
-  padding: 20px;
+  align-items: center;
+  padding: 10px;
   background: #333;
   color: white;
   font-family: 'Montserrat', sans-serif;
@@ -67,13 +72,12 @@ nav {
 nav a {
   color: white;
   text-decoration: none;
-  margin-right: 20px; 
+  margin-right: 20px;
   font-family: 'Montserrat', sans-serif;
-
 }
 
 nav a:hover {
-  color : #7dead1;
+  color : #000000;
 }
 
 nav button {
@@ -81,11 +85,22 @@ nav button {
   border: none;
   color: white;
   cursor: pointer;
-  margin-right: 20px; /* Adjust the margin as needed */
-  font-size: 14px; /* the size is applicated on Logout*/
+  margin-right: 20px;
+  font-size: 14px;
 }
 
 nav button:hover {
-  color : #7dead1;
+  color : #000000;
+}
+
+.logo-container {
+  margin-right: 20px;
+}
+
+.logo {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  object-fit: cover;
 }
 </style>
